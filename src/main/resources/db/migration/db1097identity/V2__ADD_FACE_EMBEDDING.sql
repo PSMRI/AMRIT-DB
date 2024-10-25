961b91a6dc144e7b00f17c7447a8be08b42eb57f
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2024-10-25 12:44:39 +0530

    View modified

--- src/main/resources/db/migration/db1097identity/V2__ADD_FACE_EMBEDDING.sql
+++ src/main/resources/db/migration/db1097identity/V2__ADD_FACE_EMBEDDING.sql
@@ -3,3 +3,38 @@ use db_1097_identity;
 alter table i_beneficiarydetails add ExtraFields JSON after `Others`;
  
 alter table i_beneficiarydetails add faceEmbedding longtext after Others;
+
+CREATE 
+OR REPLACE VIEW `db_1097_identity`.`v_benadvancesearch` AS
+    SELECT 
+        `benmap`.`BenMapId` AS `BenMapId`,
+        `benmap`.`BenRegId` AS `BenRegId`,
+        `benmap`.`BenDetailsId` AS `BenDetailsId`,
+        `benmap`.`BenAddressId` AS `BenAddressId`,
+        `benmap`.`BenImageId` AS `BenImageId`,
+        `benmap`.`BenContactsId` AS `BenContactsId`,
+        `benmap`.`BenConsentId` AS `BenConsentId`,
+        `benmap`.`BenAccountID` AS `BenAccountID`,
+        `benmap`.`BenSecureStackId` AS `BenSecureStackId`,
+        `benmap`.`VanSerialNo` AS `VanSerialNo`,
+        `benmap`.`VanID` AS `VanID`,
+        `benadd`.`CurrStateId` AS `CurrStateId`,
+        `benadd`.`CurrDistrictId` AS `CurrDistrictId`,
+        `benadd`.`CurrSubDistrictId` AS `CurrSubDistrictId`,
+        `benadd`.`CurrVillageId` AS `CurrVillageId`,
+        `bendetail`.`FirstName` AS `FirstName`,
+        `bendetail`.`MiddleName` AS `MiddleName`,
+        `bendetail`.`LastName` AS `LastName`,
+        `bendetail`.`GenderId` AS `GenderId`,
+        `bendetail`.`FatherName` AS `fathername`,
+        `bendetail`.`DOB` AS `DOB`,
+        `bendetailr`.`houseoldId` AS `houseoldId`,
+        `bendetailr`.`guidelineid` AS `guidelineid`,
+        `bendetailr`.`rchid` AS `rchid`
+    FROM
+        (((`db_1097_identity`.`i_beneficiarymapping` `benmap`
+        JOIN `db_1097_identity`.`i_beneficiarydetails` `bendetail` ON (((`benmap`.`BenDetailsId` = `bendetail`.`VanSerialNo`)
+            AND (`benmap`.`VanID` = `bendetail`.`VanID`))))
+        JOIN `db_1097_identity`.`i_beneficiaryaddress` `benadd` ON (((`benmap`.`BenAddressId` = `benadd`.`VanSerialNo`)
+            AND (`benmap`.`VanID` = `benadd`.`VanID`))))
+        LEFT JOIN `db_1097_identity`.`i_beneficiarydetails_rmnch` `bendetailr` ON ((`benmap`.`BenRegId` = `bendetailr`.`BeneficiaryRegID`)));
