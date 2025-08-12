d78155e83d5ecbf70ab8b243d2a6ba0095ef336a
mokirala.kumar@wipro.com <mo839639@wipro.com>
2025-08-12 12:30:43 +0530

    MHWC-19&58 performance index changes

--- src/main/resources/db/migration/dbiemr/V12__performanceindexchanges.sql
+++ src/main/resources/db/migration/dbiemr/V12__performanceindexchanges.sql
@@ -7,3 +7,22 @@ create index inx_CallDateTo on t_mctsoutboundcalls(CallDateTo);
 create index inx_preferredlanguage on t_mothervalidrecord(preferredlanguage);
 
 create index inx_preferredlanguage on t_childvaliddata(preferredlanguage);
+
+
+create index inx_villageid on i_ben_flow_outreach(villageID);
+
+create index inx_modified_date on i_ben_flow_outreach(modified_date);
+
+
+create index inx_villageid on i_ben_flow_outreach(villageID);
+
+create index inx_modified_date on i_ben_flow_outreach(modified_date);
+
+create index inx_permvillageid on db_identity.i_beneficiaryaddress(permvillageid);
+
+create index inx_Lastmoddate on db_identity.i_beneficiaryaddress(Lastmoddate);
+
+
+create index inx_Lastmoddate on db_identity.i_beneficiarymapping(Lastmoddate);
+
+create index inx_Lastmoddate on db_identity.i_beneficiarycontacts(Lastmoddate);
