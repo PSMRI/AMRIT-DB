732801febd09441e7b210e6c72123e6d099f43b9
mokirala.kumar@wipro.com <mo839639@wipro.com>
2025-08-12 14:37:14 +0530

    mhwc-19,58 coderabit suggestions

--- src/main/resources/db/migration/dbidentity/V6__identity_performanceindexchanges.sql
+++ src/main/resources/db/migration/dbidentity/V6__identity_performanceindexchanges.sql
@@ -1,11 +1,10 @@
 
-use db_identity;
 
-create index inx_permvillageid on i_beneficiaryaddress(permvillageid);
+create index inx_address_permvillageid on i_beneficiaryaddress(permvillageid);
 
-create index inx_Lastmoddate on i_beneficiaryaddress(Lastmoddate);
+create index inx_address_Lastmoddate on i_beneficiaryaddress(Lastmoddate);
 
 
-create index inx_Lastmoddate on i_beneficiarymapping(Lastmoddate);
+create index inx_mapping_Lastmoddate on i_beneficiarymapping(Lastmoddate);
 
-create index inx_Lastmoddate on i_beneficiarycontacts(Lastmoddate);
+create index inx_contacts_Lastmoddate on i_beneficiarycontacts(Lastmoddate);
