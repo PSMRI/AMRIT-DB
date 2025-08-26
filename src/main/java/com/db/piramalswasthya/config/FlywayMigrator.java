d20e36a6c040bffdf7c52537970aaa3c9b535540
Ravi Shanigarapu <ravi.shanigarapu@wipro.com>
2025-08-27 00:08:35 +0530

    Exit code 1 error fix after migration

--- src/main/java/com/db/piramalswasthya/config/FlywayMigrator.java
+++ src/main/java/com/db/piramalswasthya/config/FlywayMigrator.java
@@ -1,34 +1,42 @@
 package com.db.piramalswasthya.config;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.db.piramalswasthya.controller.VersionController;
 
 import jakarta.annotation.PostConstruct;
 
 @Component
 public class FlywayMigrator {
-	@Autowired
-	private Flyway flywaydbiemr;
-	
-	@Autowired
-	private Flyway flywaydbidentity;
-	
-	@Autowired
-	private Flyway flywaydbreporting;
-	
-	@Autowired
-	private Flyway flywaydb1097identity;
-	@PostConstruct
-	public void migrate() {
-		try {
-			flywaydbiemr.migrate();
-			flywaydbidentity.migrate();
-			flywaydbreporting.migrate();
-			flywaydb1097identity.migrate();
-		}catch (Exception e) {
-			throw new RuntimeException("Flyway Migration Failed ",e);
-		}
-	}
+	private Logger logger = LoggerFactory.getLogger(FlywayMigrator.class);
+	private final Flyway flywayDbiemr;
+    private final Flyway flywayDbidentity;
+    private final Flyway flywayDbreporting;
+    private final Flyway flywayDb1097identity;
+
+    public FlywayMigrator(Flyway flywayDbiemr,
+                          Flyway flywayDbidentity,
+                          Flyway flywayDbreporting,
+                          Flyway flywayDb1097identity) {
+        this.flywayDbiemr = flywayDbiemr;
+        this.flywayDbidentity = flywayDbidentity;
+        this.flywayDbreporting = flywayDbreporting;
+        this.flywayDb1097identity = flywayDb1097identity;
+    }
+
+    @PostConstruct
+    public void migrate() {
+        flywayDbiemr.migrate();
+        flywayDbidentity.migrate();
+        flywayDbreporting.migrate();
+        flywayDb1097identity.migrate();
+        System.out.println("SUCCESS");
+        logger.info("Flyway migration completed successfully");
+        
+    }
 }
 
--- src/main/java/com/db/piramalswasthya/service/VersionService.java
+++ src/main/java/com/db/piramalswasthya/service/VersionService.java
@@ -22,46 +22,44 @@
 */
 package com.db.piramalswasthya.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.db.piramalswasthya.data.FlywaySchemaVersion;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
 @Repository
 public class VersionService {
-	
-    private JdbcTemplate jdbcTemplate;
-	
-	@Autowired
-	public void setCommonServiceImpl(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+
+	private final JdbcTemplate dbiemrJdbcTemplate;
+
+	public VersionService(@Qualifier("dbiemrJdbcTemplate") JdbcTemplate jdbcTemplate) {
+		this.dbiemrJdbcTemplate = jdbcTemplate;
 	}
-	
+
 	public List<FlywaySchemaVersion> getLatestVersion() {
 		String sql = "SELECT * FROM flyway_schema_history order by 1 desc limit 1";
-        return jdbcTemplate.query(sql, new RowMapper<FlywaySchemaVersion>() {
-            @Override
-            public FlywaySchemaVersion mapRow(ResultSet rs, int rowNum) throws SQLException {
-            	FlywaySchemaVersion history = new FlywaySchemaVersion();
-                history.setInstalledRank(rs.getInt("installed_rank"));
-                history.setVersion(rs.getString("version"));
-                history.setDescription(rs.getString("description"));
-                history.setType(rs.getString("type"));
-                history.setScript(rs.getString("script"));
-                history.setChecksum(rs.getInt("checksum"));
-                history.setInstalledBy(rs.getString("installed_by"));
-                history.setInstalledOn(rs.getTimestamp("installed_on"));
-                history.setExecutionTime(rs.getInt("execution_time"));
-                history.setSuccess(rs.getBoolean("success"));
-                return history;
-            }
-        });
-    }
+		return dbiemrJdbcTemplate.query(sql, new RowMapper<FlywaySchemaVersion>() {
+			@Override
+			public FlywaySchemaVersion mapRow(ResultSet rs, int rowNum) throws SQLException {
+				FlywaySchemaVersion history = new FlywaySchemaVersion();
+				history.setInstalledRank(rs.getInt("installed_rank"));
+				history.setVersion(rs.getString("version"));
+				history.setDescription(rs.getString("description"));
+				history.setType(rs.getString("type"));
+				history.setScript(rs.getString("script"));
+				history.setChecksum(rs.getInt("checksum"));
+				history.setInstalledBy(rs.getString("installed_by"));
+				history.setInstalledOn(rs.getTimestamp("installed_on"));
+				history.setExecutionTime(rs.getInt("execution_time"));
+				history.setSuccess(rs.getBoolean("success"));
+				return history;
+			}
+		});
+	}
 }
