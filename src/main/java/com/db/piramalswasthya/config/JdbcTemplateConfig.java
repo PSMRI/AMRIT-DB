85218c5c5b1c4a623fe42c2470b9cf7b964f9826
ravishanigarapu <133210792+ravishanigarapu@users.noreply.github.com>
2025-08-29 11:59:39 +0530

    Exit code 1 error fix after migration (#35)
    * Exit code 1 error fix after migration
    
    * Coderabbit comments addressed
    
    * Jboss file added

--- src/main/java/com/db/piramalswasthya/config/JdbcTemplateConfig.java
+++ src/main/java/com/db/piramalswasthya/config/JdbcTemplateConfig.java
@@ -2,38 +2,31 @@ package com.db.piramalswasthya.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class JdbcTemplateConfig {
 	
-	@Value("${spring.datasource.dbiemr.url}")
-    private String dbiemrurl;
-	
-	@Value("${spring.datasource.dbiemr.username}")
-    private String username;
-	
-	@Value("${spring.datasource.dbiemr.password}")
-    private String password;
-	
-	@Value("${spring.datasource.dbiemr.driver-class-name}")
-    private String driverUrl;
 	@Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverUrl);
-        dataSource.setUrl(dbiemrurl);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
+    public JdbcTemplate dbiemrJdbcTemplate(@Qualifier("dbiemrDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+    public JdbcTemplate dbidentityJdbcTemplate(@Qualifier("dbidentityDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    public JdbcTemplate dbreportingJdbcTemplate(@Qualifier("dbreportingDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
+    }
+
+    @Bean
+    public JdbcTemplate db1097identityJdbcTemplate(@Qualifier("db1097identityDataSource") DataSource ds) {
+        return new JdbcTemplate(ds);
     }
 }
