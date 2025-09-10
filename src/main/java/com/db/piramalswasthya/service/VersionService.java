299aa5cbb96a73ef87c42bd34f80a4069bc7a07d
ravishanigarapu <133210792+ravishanigarapu@users.noreply.github.com>
2025-09-10 09:57:44 +0530

    To get the all the latest migrations version (#38)
    * Exit code 1 error fix after migration
    
    * Coderabbit comments addressed
    
    * Jboss file added
    
    * getting all the latest versions

--- src/main/java/com/db/piramalswasthya/service/VersionService.java
+++ src/main/java/com/db/piramalswasthya/service/VersionService.java
@@ -24,7 +24,9 @@ package com.db.piramalswasthya.service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -37,29 +39,47 @@ import com.db.piramalswasthya.data.FlywaySchemaVersion;
 public class VersionService {
 
 	private final JdbcTemplate dbiemrJdbcTemplate;
+	private final JdbcTemplate dbreportingJdbcTemplate;
+	private final JdbcTemplate dbidentityJdbcTemplate;
+	private final JdbcTemplate db1097identityJdbcTemplate;
 
-	public VersionService(@Qualifier("dbiemrJdbcTemplate") JdbcTemplate jdbcTemplate) {
-		this.dbiemrJdbcTemplate = jdbcTemplate;
+	public VersionService(@Qualifier("dbiemrJdbcTemplate") JdbcTemplate dbiemrJdbcTemplate,
+			@Qualifier("dbreportingJdbcTemplate") JdbcTemplate dbreportingJdbcTemplate,
+			@Qualifier("dbidentityJdbcTemplate") JdbcTemplate dbidentityJdbcTemplate,
+			@Qualifier("db1097identityJdbcTemplate") JdbcTemplate db1097identityJdbcTemplate) {
+		this.dbiemrJdbcTemplate = dbiemrJdbcTemplate;
+		this.dbreportingJdbcTemplate = dbreportingJdbcTemplate;
+		this.dbidentityJdbcTemplate = dbidentityJdbcTemplate;
+		this.db1097identityJdbcTemplate = db1097identityJdbcTemplate;
+		
 	}
 
-	public List<FlywaySchemaVersion> getLatestVersion() {
+	
+	public Map<String,List<FlywaySchemaVersion>> getAllLatestVersion() {
+		Map<String,List<FlywaySchemaVersion>> result = new HashMap<>();
+		result.put("db_iemr", getLatestVersion(dbiemrJdbcTemplate));
+		result.put("db_reporting", getLatestVersion(dbreportingJdbcTemplate));
+		result.put("db_identity", getLatestVersion(dbidentityJdbcTemplate));
+		result.put("db_1097_identity", getLatestVersion(db1097identityJdbcTemplate));
+		
+		return result;
+	}
+	private List<FlywaySchemaVersion> getLatestVersion(JdbcTemplate jdbcTemplate){
 		String sql = "SELECT * FROM flyway_schema_history order by 1 desc limit 1";
-		return dbiemrJdbcTemplate.query(sql, new RowMapper<FlywaySchemaVersion>() {
-			@Override
-			public FlywaySchemaVersion mapRow(ResultSet rs, int rowNum) throws SQLException {
-				FlywaySchemaVersion history = new FlywaySchemaVersion();
-				history.setInstalledRank(rs.getInt("installed_rank"));
-				history.setVersion(rs.getString("version"));
-				history.setDescription(rs.getString("description"));
-				history.setType(rs.getString("type"));
-				history.setScript(rs.getString("script"));
-				history.setChecksum(rs.getObject("checksum", Integer.class));
-				history.setInstalledBy(rs.getString("installed_by"));
-				history.setInstalledOn(rs.getTimestamp("installed_on"));
-				history.setExecutionTime(rs.getInt("execution_time"));
-				history.setSuccess(rs.getBoolean("success"));
-				return history;
-			}
+		return jdbcTemplate.query(sql, (rs, rowNum)->{
+			FlywaySchemaVersion history = new FlywaySchemaVersion();
+			history.setInstalledRank(rs.getInt("installed_rank"));
+			history.setVersion(rs.getString("version"));
+			history.setDescription(rs.getString("description"));
+			history.setType(rs.getString("type"));
+			history.setScript(rs.getString("script"));
+			history.setChecksum(rs.getObject("checksum", Integer.class));
+			history.setInstalledBy(rs.getString("installed_by"));
+			history.setInstalledOn(rs.getTimestamp("installed_on"));
+			history.setExecutionTime(rs.getInt("execution_time"));
+			history.setSuccess(rs.getBoolean("success"));
+			return history;
+
 		});
 	}
 }
