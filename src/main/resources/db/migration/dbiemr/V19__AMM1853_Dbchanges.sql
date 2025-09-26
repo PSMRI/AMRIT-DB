e09dfa56bc0700918717abf2631eecaadea9cc53
mokirala.kumar@wipro.com <mo839639@wipro.com>
2025-09-26 13:15:47 +0530

    amm-1847,1853 db changes

---
+++ src/main/resources/db/migration/dbiemr/V19__AMM1853_Dbchanges.sql
@@ -0,0 +1,8 @@
+
+
+
+alter table t_pmsma add visitDate Datetime Default null;
+ 
+alter table t_pmsma add visitNo smallint Default null;
+ 
+alter table t_pmsma add anyOtherHighRiskCondition varchar(100) Default null;
---
+++ src/main/resources/db/migration/dbiemr/V20__AMM1847_Cbacdbchanges.sql
@@ -0,0 +1,7 @@
+
+
+alter table t_cbacdetails add Cbac_OccupationalExposure varchar(100);
+alter table t_cbacdetails add Cbac_BotheredProblem_last2weeks varchar(100);
+alter table t_cbacdetails add Cbac_LittleInterest_Pleasure varchar(100);
+alter table t_cbacdetails add Cbac_Depressed_hopeless varchar(100);
+alter table t_cbacdetails add Cbac_DiscolorationSkin varchar(100);
