3e77a0c1a8e5a10411233357b48f00a9027262c4
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-10-07 11:04:43 +0530

    fix mapping fk issue

--- src/main/resources/db/migration/dbiemr/V20__DB_IMER_incentive_lang.sql
+++ src/main/resources/db/migration/dbiemr/V20__DB_IMER_incentive_lang.sql
@@ -5,6 +5,29 @@ use db_iemr;
 --
 -- Table structure for table `m_incentive_activity_lang_mapping`
 --
+CREATE TABLE `m_incentive_activity` (
+   `id` bigint NOT NULL AUTO_INCREMENT,
+   `created_by` varchar(255) DEFAULT NULL,
+   `created_date` datetime DEFAULT NULL,
+   `activity_description` varchar(255) DEFAULT NULL,
+   `district_code` int DEFAULT NULL,
+   `group_name` varchar(255) DEFAULT NULL,
+   `is_deleted` bit(1) DEFAULT NULL,
+   `activity_name` varchar(255) DEFAULT NULL,
+   `payment_parameter` varchar(255) DEFAULT NULL,
+   `rate_per_activity` int DEFAULT NULL,
+   `state_code` int DEFAULT NULL,
+   `updated_by` varchar(255) DEFAULT NULL,
+   `updated_date` datetime DEFAULT NULL,
+   `fmr_code` varchar(255) DEFAULT NULL,
+   `fmr_code_old` varchar(255) DEFAULT NULL,
+   `location` varchar(200) DEFAULT NULL,
+   `state_activity_code` varchar(100) DEFAULT NULL,
+   `role_id` varchar(100) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB AUTO_INCREMENT=141 DEFAULT CHARSET=utf8mb3;
+
+
 CREATE TABLE `m_incentive_activity_lang_mapping` (
    `id` bigint NOT NULL AUTO_INCREMENT,
    `created_date` datetime DEFAULT NULL,
@@ -47,27 +70,5 @@ CREATE TABLE `m_incentive_activity_rank_mapping` (
         CHECK (`rank_order` >= 0)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-CREATE TABLE `m_incentive_activity` (
-   `id` bigint NOT NULL AUTO_INCREMENT,
-   `created_by` varchar(255) DEFAULT NULL,
-   `created_date` datetime DEFAULT NULL,
-   `activity_description` varchar(255) DEFAULT NULL,
-   `district_code` int DEFAULT NULL,
-   `group_name` varchar(255) DEFAULT NULL,
-   `is_deleted` bit(1) DEFAULT NULL,
-   `activity_name` varchar(255) DEFAULT NULL,
-   `payment_parameter` varchar(255) DEFAULT NULL,
-   `rate_per_activity` int DEFAULT NULL,
-   `state_code` int DEFAULT NULL,
-   `updated_by` varchar(255) DEFAULT NULL,
-   `updated_date` datetime DEFAULT NULL,
-   `fmr_code` varchar(255) DEFAULT NULL,
-   `fmr_code_old` varchar(255) DEFAULT NULL,
-   `location` varchar(200) DEFAULT NULL,
-   `state_activity_code` varchar(100) DEFAULT NULL,
-   `role_id` varchar(100) DEFAULT NULL,
-   PRIMARY KEY (`id`)
- ) ENGINE=InnoDB AUTO_INCREMENT=141 DEFAULT CHARSET=utf8mb3;
 
 
