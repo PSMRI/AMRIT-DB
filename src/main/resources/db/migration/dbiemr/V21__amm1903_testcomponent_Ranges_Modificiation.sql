51dd9fa8f2768c6dc8ce55e29b036365508766c7
mokirala.kumar@wipro.com <mo839639@wipro.com>
2025-10-21 18:15:50 +0530

    AMM 1903 db changes

---
+++ src/main/resources/db/migration/dbiemr/V21__amm1903_testcomponent_Ranges_Modificiation.sql
@@ -0,0 +1,6 @@
+
+
+
+alter table m_testcomponent modify Range_normal_min decimal(10,2),
+                          modify Range_normal_max decimal(10,2),
+                           modify Range_max decimal(10,2);
