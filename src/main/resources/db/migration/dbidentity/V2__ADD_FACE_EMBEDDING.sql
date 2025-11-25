8cac32abab5430e5b9dd6352bf73daa4a731ee40
unknown <paras@navadhiti.com>
2025-11-25 11:59:50 +0530

    fix:changes in face embedding sql file version 2

--- src/main/resources/db/migration/dbidentity/V2__ADD_FACE_EMBEDDING.sql
+++ src/main/resources/db/migration/dbidentity/V2__ADD_FACE_EMBEDDING.sql
@@ -2,6 +2,7 @@
 
 use db_identity;
 
-alter table i_beneficiarydetails add ExtraFields JSON after `Others`;
+-- make ALTER add operations idempotent: only add columns if they do NOT already exist
+alter table i_beneficiarydetails add column if not exists ExtraFields JSON after `Others`;
  
-alter table i_beneficiarydetails add faceEmbedding longtext after Others;
+alter table i_beneficiarydetails add column if not exists faceEmbedding longtext after `Others`;
