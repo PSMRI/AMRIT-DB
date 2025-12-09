facda199c00dc6725d84c1e31b42a165148e0e1c
unknown <paras@navadhiti.com>
2025-12-09 10:33:48 +0530

    fix:t_idrdetails column modification

---
+++ src/main/resources/db/migration/dbiemr/V23_DB_IMER_t_idrsdetails.sql
@@ -0,0 +1,8 @@
+-- Changes in table definition for t_idrsdetails
+
+ALTER TABLE t_idrsdetails 
+MODIFY COLUMN DiseaseQuestionType LONGTEXT;
+
+
+ALTER TABLE t_idrsdetails 
+ MODIFY COLUMN Question LONGTEXT;
