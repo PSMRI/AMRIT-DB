e8131e359fbf43b05ba4a2211525643f82d7adeb
Vanitha S <116701245+vanitha1822@users.noreply.github.com>
2025-12-10 11:24:37 +0530

    fix: Update Stored Procedure (#49)

---
+++ src/main/resources/db/migration/dbiemr/V24__AMM1660_Call_History.sql
@@ -0,0 +1,32 @@
+USE `db_iemr`;
+DROP procedure IF EXISTS `Pr_GetCallHistory`;
+ 
+USE `db_iemr`;
+DROP procedure IF EXISTS `db_iemr`.`Pr_GetCallHistory`;
+;
+ 
+DELIMITER $$
+USE `db_iemr`$$
+CREATE PROCEDURE `Pr_GetCallHistory`(v_obcallid int(11))
+begin
+select t1.callid,t2.MotherID,t3.Name,null Child_id,null child_name,
+t1.PhoneNo,t3.ASHA_Name,t1.ReceivedRoleName,t3.LMP_Date,null dob,
+t1.calltime,t2.OutboundCallType "ECD CallType",t1.TypeOfComplaint ,
+t1.complaintRemarks,t1.Remarks "AdviceProvided" from 
+(select * from t_bencall t1 where obcallid=ifnull(v_obcallid,t1.obcallid))t1
+inner join t_mctsoutboundcalls t2 on t1.obcallid=t2.obcallid
+inner join t_mothervalidrecord t3 on t2.motherID=t3.MCTSID_no
+where t1.obcallid=ifnull(v_obcallid,t1.obcallid)
+union
+select t1.callid,t2.MotherID,t3.Mother_Name,t3.MCTSID_no_Child_ID Child_id,t3.Child_Name child_name,
+t1.PhoneNo,t3.ASHA_Name,t1.ReceivedRoleName,null LMP_Date,t3.DOB dob,
+t1.calltime,t2.OutboundCallType "ECD CallType",t1.TypeOfComplaint ,
+t1.complaintRemarks,t1.Remarks "AdviceProvided" from 
+(select * from t_bencall t1 where obcallid=ifnull(v_obcallid,t1.obcallid)) t1
+inner join t_mctsoutboundcalls t2 on t1.obcallid=t2.obcallid
+inner join t_childvaliddata t3 on t2.childID=t3.MCTSID_no_Child_ID
+where t1.obcallid=ifnull(v_obcallid,t1.obcallid);
+end$$
+ 
+DELIMITER ;
+;
