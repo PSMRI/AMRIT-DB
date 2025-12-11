952f4df334b0b94e5747c7a733173afc6ce02861
unknown <paras@navadhiti.com>
2025-12-11 10:05:16 +0530

    fix:Modify table's column and added SP

--- src/main/resources/db/migration/dbiemr/V23_DB_IMER_t_idrsdetails.sql
+++
@@ -1,8 +0,0 @@
--- Changes in table definition for t_idrsdetails
-
-ALTER TABLE t_idrsdetails 
-MODIFY COLUMN DiseaseQuestionType LONGTEXT;
-
-
-ALTER TABLE t_idrsdetails 
- MODIFY COLUMN Question LONGTEXT;
