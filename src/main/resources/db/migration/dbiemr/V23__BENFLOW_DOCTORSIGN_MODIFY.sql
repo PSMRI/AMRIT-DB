19926fe837b8aa38d8e9ca3647d6a2401ffa3beb
unknown <paras@navadhiti.com>
2025-12-16 17:56:46 +0530

    fix:changes for assam UAT

--- src/main/resources/db/migration/dbiemr/V23__BENFLOW_DOCTORSIGN_MODIFY.sql
+++ src/main/resources/db/migration/dbiemr/V23__BENFLOW_DOCTORSIGN_MODIFY.sql
@@ -1,5 +1,5 @@
 
 
 
--- ALTER TABLE db_iemr.i_ben_flow_outreach   
--- ADD COLUMN doctor_signature_flag BOOLEAN DEFAULT FALSE;
+ALTER TABLE db_iemr.i_ben_flow_outreach   
+ADD COLUMN doctor_signature_flag BOOLEAN DEFAULT FALSE;
