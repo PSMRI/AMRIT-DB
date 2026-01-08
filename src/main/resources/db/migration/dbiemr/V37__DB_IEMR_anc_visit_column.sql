0c11b3b826f63cdc5e6dee03b1b055a5fec19f21
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-01-08 10:54:14 +0530

    fix query

--- src/main/resources/db/migration/dbiemr/V37__DB_IEMR_anc_visit_column.sql
+++ src/main/resources/db/migration/dbiemr/V37__DB_IEMR_anc_visit_column.sql
@@ -1,33 +1,23 @@
 USE db_iemr;
 
 ALTER TABLE t_anc_visit
-ADD COLUMN IF NOT EXISTS file_path VARCHAR(255),
-ADD COLUMN IF NOT EXISTS serial_no VARCHAR(100),
-
-ADD COLUMN IF NOT EXISTS method_of_termination VARCHAR(100),
-ADD COLUMN IF NOT EXISTS method_of_termination_id INT,
-
-ADD COLUMN IF NOT EXISTS termination_done_by VARCHAR(100),
-ADD COLUMN IF NOT EXISTS termination_done_by_id INT,
-
-ADD COLUMN IF NOT EXISTS is_paiucd_id INT,
-ADD COLUMN IF NOT EXISTS is_paiucd VARCHAR(10),
-
-ADD COLUMN IF NOT EXISTS remarks TEXT,
-
-ADD COLUMN IF NOT EXISTS abortion_img1 VARCHAR(255),
-ADD COLUMN IF NOT EXISTS abortion_img2 VARCHAR(255),
-
-ADD COLUMN IF NOT EXISTS place_of_death VARCHAR(100),
-ADD COLUMN IF NOT EXISTS place_of_death_id INT,
-ADD COLUMN IF NOT EXISTS other_place_of_death VARCHAR(255),
-
-ADD COLUMN IF NOT EXISTS visit_date TIMESTAMP,
-ADD COLUMN IF NOT EXISTS lmp_date TIMESTAMP,
-
-ADD COLUMN IF NOT EXISTS is_YesOrNo BOOLEAN,
-
-ADD COLUMN IF NOT EXISTS date_of_sterilisation TIMESTAMP,
-
-ADD COLUMN IF NOT EXISTS place_of_anc VARCHAR(100),
-ADD COLUMN IF NOT EXISTS place_of_ancId INT;
+ADD COLUMN file_path VARCHAR(255),
+ADD COLUMN serial_no VARCHAR(100),
+ADD COLUMN method_of_termination VARCHAR(100),
+ADD COLUMN method_of_termination_id INT,
+ADD COLUMN termination_done_by VARCHAR(100),
+ADD COLUMN termination_done_by_id INT,
+ADD COLUMN is_paiucd_id INT,
+ADD COLUMN is_paiucd VARCHAR(10),
+ADD COLUMN remarks TEXT,
+ADD COLUMN abortion_img1 VARCHAR(255),
+ADD COLUMN abortion_img2 VARCHAR(255),
+ADD COLUMN place_of_death VARCHAR(100),
+ADD COLUMN place_of_death_id INT,
+ADD COLUMN other_place_of_death VARCHAR(255),
+ADD COLUMN visit_date TIMESTAMP,
+ADD COLUMN lmp_date TIMESTAMP,
+ADD COLUMN is_YesOrNo BOOLEAN,
+ADD COLUMN date_of_sterilisation TIMESTAMP,
+ADD COLUMN place_of_anc VARCHAR(100),
+ADD COLUMN place_of_ancId INT;
