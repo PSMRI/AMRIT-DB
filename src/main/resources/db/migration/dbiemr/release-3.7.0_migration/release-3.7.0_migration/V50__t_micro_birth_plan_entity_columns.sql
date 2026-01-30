bbb8c6eb0b988054df7824c0237e0c836c142580
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-01-30 12:47:07 +0530

    migration for release 3.7.0

--- src/main/resources/db/migration/dbiemr/release-3.7.0_migration/release-3.7.0_migration/V50__t_micro_birth_plan_entity_columns.sql
+++
@@ -1,5 +0,0 @@
-USE db_iemr;
-
--- Align t_micro_birth_plan with MicroBirthPlan entity: community_support_contact length 100
-ALTER TABLE db_iemr.t_micro_birth_plan
-  MODIFY COLUMN community_support_contact VARCHAR(100) DEFAULT NULL;
--- src/main/resources/db/migration/dbiemr/release-3.7.0_migration/release-3.7.0_migration/V51__adolescent_health_entity_columns.sql
+++
@@ -1,5 +0,0 @@
-USE db_iemr;
-
--- adolescent_health: visit_date DATE -> DATETIME (entity Timestamp)
-ALTER TABLE db_iemr.adolescent_health
-  MODIFY COLUMN visit_date DATETIME NULL DEFAULT NULL;
