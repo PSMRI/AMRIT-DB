42e6359a8278a9cfb84bdd1332595e9ae96f7e62
Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>
2026-02-13 16:19:24 +0530

    add auto_increment on id feild (#92)
    * add auto_increment on id feild
    
    * add auto_increment on id feild
    
    * add auto_increment on id feild
    
    * add auto_increment on id feild
    
    * remove duplicate db_iemr

--- src/main/resources/db/migration/dbiemr/V60__add_auto_increment_in_screening_DBIEMR.sql
+++ src/main/resources/db/migration/dbiemr/V60__add_auto_increment_in_screening_DBIEMR.sql
@@ -1,56 +1,70 @@
 USE db_iemr;
 
+ALTER TABLE screening_kala_azar
+MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE screening_filaria
+MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE screening_aesje
+MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE irs_round
+MODIFY COLUMN round_number INT NULL,
+MODIFY COLUMN round_date TIMESTAMP NULL,
+MODIFY COLUMN updated_by VARCHAR(200) NULL;
+
 SET @dbname = 'db_iemr';
+SET @tablename = 'irs_round';
 
-SET @tablename = 'screening_kala_azar';
-
-SET @preparedStatement = (
-SELECT IF(
- (SELECT EXTRA
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA=@dbname
-  AND TABLE_NAME=@tablename
-  AND COLUMN_NAME='id') LIKE '%auto_increment%',
+SET @columnname = 'irs_date';
+SET @preparedStatement = (SELECT IF(
+ (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA=@dbname AND TABLE_NAME=@tablename AND COLUMN_NAME=@columnname) > 0,
  'SELECT 1',
- CONCAT('ALTER TABLE ', @dbname, '.', @tablename,
-        ' MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;')
+ CONCAT('ALTER TABLE ',@dbname,'.',@tablename,
+        ' ADD COLUMN irs_date DATE DEFAULT NULL;')
 ));
-PREPARE stmt FROM @preparedStatement;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+PREPARE stmt FROM @preparedStatement; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 
-SET @tablename = 'screening_filaria';
-
-SET @preparedStatement = (
-SELECT IF(
- (SELECT EXTRA
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA=@dbname
-  AND TABLE_NAME=@tablename
-  AND COLUMN_NAME='id') LIKE '%auto_increment%',
+SET @columnname = 'round';
+SET @preparedStatement = (SELECT IF(
+ (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA=@dbname AND TABLE_NAME=@tablename AND COLUMN_NAME=@columnname) > 0,
  'SELECT 1',
- CONCAT('ALTER TABLE ', @dbname, '.', @tablename,
-        ' MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;')
+ CONCAT('ALTER TABLE ',@dbname,'.',@tablename,
+        ' ADD COLUMN round INT DEFAULT NULL;')
 ));
-PREPARE stmt FROM @preparedStatement;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+PREPARE stmt FROM @preparedStatement; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 
-SET @tablename = 'screening_aesje';
-
-SET @preparedStatement = (
-SELECT IF(
- (SELECT EXTRA
-  FROM INFORMATION_SCHEMA.COLUMNS
-  WHERE TABLE_SCHEMA=@dbname
-  AND TABLE_NAME=@tablename
-  AND COLUMN_NAME='id') LIKE '%auto_increment%',
+SET @columnname = 'householdId';
+SET @preparedStatement = (SELECT IF(
+ (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA=@dbname AND TABLE_NAME=@tablename AND COLUMN_NAME=@columnname) > 0,
  'SELECT 1',
- CONCAT('ALTER TABLE ', @dbname, '.', @tablename,
-        ' MODIFY id BIGINT(20) NOT NULL AUTO_INCREMENT;')
+ CONCAT('ALTER TABLE ',@dbname,'.',@tablename,
+        ' ADD COLUMN householdId BIGINT DEFAULT NULL;')
 ));
-PREPARE stmt FROM @preparedStatement;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+PREPARE stmt FROM @preparedStatement; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+
+SET @columnname = 'created_by';
+SET @preparedStatement = (SELECT IF(
+ (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA=@dbname AND TABLE_NAME=@tablename AND COLUMN_NAME=@columnname) > 0,
+ 'SELECT 1',
+ CONCAT('ALTER TABLE ',@dbname,'.',@tablename,
+        ' ADD COLUMN created_by VARCHAR(255) DEFAULT NULL;')
+));
+PREPARE stmt FROM @preparedStatement; EXECUTE stmt; DEALLOCATE PREPARE stmt;
+
+SET @columnname = 'user_id';
+SET @preparedStatement = (SELECT IF(
+ (SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+  WHERE TABLE_SCHEMA=@dbname AND TABLE_NAME=@tablename AND COLUMN_NAME=@columnname) > 0,
+ 'SELECT 1',
+ CONCAT('ALTER TABLE ',@dbname,'.',@tablename,
+        ' ADD COLUMN user_id INT DEFAULT NULL;')
+));
+PREPARE stmt FROM @preparedStatement; EXECUTE stmt; DEALLOCATE PREPARE stmt;
