6b107852b3a602c681fd592ee5cba7da5fa38f11
KadariPavani <kadaripavani1@gmail.com>
2026-02-26 13:05:45 +0530

    fix: skip reapplying successful Flyway migrations when checksum changes #117

--- src/main/java/com/db/piramalswasthya/config/FlywayConfig.java
+++ src/main/java/com/db/piramalswasthya/config/FlywayConfig.java
@@ -3,46 +3,64 @@ package com.db.piramalswasthya.config;
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FlywayConfig {
+
+	@Value("${amrit.flyway.ignore-applied-migration-checksum:false}")
+	private boolean ignoreAppliedMigrationChecksum;
+
 	 @Bean
 	    public Flyway flywayDbiemr(@Qualifier("dbiemrDataSource") DataSource dataSource) {
-	        return Flyway.configure()
+	        FluentConfiguration config = Flyway.configure()
 	                .dataSource(dataSource)
 	                .locations("classpath:db/migration/dbiemr")
-	                .baselineOnMigrate(true)
-	                .load();
+	                .baselineOnMigrate(true);
+	        if (ignoreAppliedMigrationChecksum) {
+	            config.ignoreMigrationPatterns("*:applied");
+	        }
+	        return config.load();
 	    }
 
 	    @Bean
 	    public Flyway flywayDbidentity(@Qualifier("dbidentityDataSource") DataSource dataSource) {
-	        return Flyway.configure()
+	        FluentConfiguration config = Flyway.configure()
 	                .dataSource(dataSource)
 	                .locations("classpath:db/migration/dbidentity")
-	                .baselineOnMigrate(true)
-	                .load();
+	                .baselineOnMigrate(true);
+	        if (ignoreAppliedMigrationChecksum) {
+	            config.ignoreMigrationPatterns("*:applied");
+	        }
+	        return config.load();
 	    }
 
 	    @Bean
 	    public Flyway flywayDbreporting(@Qualifier("dbreportingDataSource") DataSource dataSource) {
-	        return Flyway.configure()
+	        FluentConfiguration config = Flyway.configure()
 	                .dataSource(dataSource)
 	                .locations("classpath:db/migration/dbreporting")
-	                .baselineOnMigrate(true)
-	                .load();
+	                .baselineOnMigrate(true);
+	        if (ignoreAppliedMigrationChecksum) {
+	            config.ignoreMigrationPatterns("*:applied");
+	        }
+	        return config.load();
 	    }
 
 	    @Bean
 	    public Flyway flywayDb1097identity(@Qualifier("db1097identityDataSource") DataSource dataSource) {
-	        return Flyway.configure()
+	        FluentConfiguration config = Flyway.configure()
 	                .dataSource(dataSource)
 	                .locations("classpath:db/migration/db1097identity")
-	                .baselineOnMigrate(true)
-	                .load();
+	                .baselineOnMigrate(true);
+	        if (ignoreAppliedMigrationChecksum) {
+	            config.ignoreMigrationPatterns("*:applied");
+	        }
+	        return config.load();
 	    }
 
 }
