eade7ed8d831185fc1e2af2393ba22f7e5efe23b
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-05-13 16:05:06 +0530

    migration of bengali translation

---
+++ src/main/resources/db/migration/dbiemr/V76__translation_migration.sql
@@ -0,0 +1,37 @@
+USE db_iemr;
+
+SET @column_exists = (
+
+    SELECT COUNT(*) 
+    FROM INFORMATION_SCHEMA.COLUMNS 
+    WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = 'm_translation'
+      AND COLUMN_NAME = 'label_bn'
+);
+
+SET @sql = IF(@column_exists = 0,
+    'ALTER TABLE m_translation ADD COLUMN label_bn varchar(500) NULL DEFAULT NULL',
+    'SELECT "Column already exists"'
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+
+SET @column_exists = (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = DATABASE()
+      AND TABLE_NAME = 'm_translation'
+      AND COLUMN_NAME = 'bengali_translation'
+);
+
+SET @sql = IF(@column_exists = 0,
+    'ALTER TABLE m_translation ADD COLUMN bengali_translation varchar(500) NULL DEFAULT NULL',
+    'SELECT "Column already exists"'
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
