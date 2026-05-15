79793464683932e22155ac90f4d8a3a2cc6783cf
Saurav Mishra <80103738+SauravBizbRolly@users.noreply.github.com>
2026-05-15 15:38:19 +0530

    Migration/asha supervisor (#125)
    * migration for option key
    
    * migration of asha supervisor
    
    * migration of asha supervisor
    
    * migration of asha supervisor

--- src/main/resources/db/migration/dbiemr/V76__translation_migration.sql
+++ src/main/resources/db/migration/dbiemr/V76__translation_migration.sql
@@ -5,12 +5,12 @@ SET @column_exists = (
     SELECT COUNT(*) 
     FROM INFORMATION_SCHEMA.COLUMNS 
     WHERE TABLE_SCHEMA = DATABASE()
-      AND TABLE_NAME = 'm_translation'
+      AND TABLE_NAME = 'form_field_options'
       AND COLUMN_NAME = 'label_bn'
 );
 
 SET @sql = IF(@column_exists = 0,
-    'ALTER TABLE m_translation ADD COLUMN label_bn varchar(500) NULL DEFAULT NULL',
+    'ALTER TABLE form_field_options ADD COLUMN label_bn varchar(500) NULL DEFAULT NULL',
     'SELECT "Column already exists"'
 );
 
