bf76770fc613b17b2b22d5f9654ea861558fd767
Paras_dba <241824862+paras-dba@users.noreply.github.com>
2026-06-24 16:04:12 +0530

    fix_view

---
+++ src/main/resources/db/migration/dbiemr/V83__DB_view_correction.sql
@@ -0,0 +1,41 @@
+drop view v_fetchfacility;
+
+CREATE 
+    ALGORITHM = UNDEFINED 
+    DEFINER = `piramaldev`@`%` 
+    SQL SECURITY DEFINER
+VIEW `v_fetchfacility` AS
+    SELECT 
+        `mf`.`FacilityID` AS `FacilityID`,
+        `mf`.`FacilityName` AS `FacilityName`,
+        `mf`.`FacilityDesc` AS `FacilityDesc`,
+        `mf`.`FacilityCode` AS `FacilityCode`,
+        `mf`.`FacilityTypeID` AS `FacilityTypeID`,
+        `mf`.`Location` AS `Location`,
+        `mf`.`PhysicalLocation` AS `PhysicalLocation`,
+        `mf`.`StoreType` AS `StoreType`,
+        `mf`.`Status` AS `Status`,
+        `mf`.`ProviderServiceMapID` AS `ProviderServiceMapID`,
+        `mf`.`IsMainFacility` AS `IsMainFacility`,
+        `mf`.`MainFacilityID` AS `MainFacilityID`,
+        `mf`.`Deleted` AS `FacilityDeleted`,
+        `mf`.`CreatedBy` AS `FacilityCreatedBy`,
+        `mf`.`CreatedDate` AS `FacilityCreatedDate`,
+        `mf`.`ModifiedBy` AS `FacilityModifiedBy`,
+        `mf`.`LastModDate` AS `FacilityLastModDate`,
+        `mp`.`ParkingPlaceID` AS `ParkingPlaceID`,
+        `mp`.`ParkingPlaceName` AS `ParkingPlaceName`,
+        `mp`.`ParkingPlaceDesc` AS `ParkingPlaceDesc`,
+        `mp`.`AreaHQAddress` AS `AreaHQAddress`,
+        `mp`.`Deleted` AS `PPDeleted`,
+        `mv`.`VanID` AS `VanID`,
+        `mv`.`VanName` AS `VanName`,
+        `mv`.`VehicalNo` AS `VehicalNo`,
+        `mv`.`VanTypeID` AS `VanTypeID`,
+        `mv`.`ParkingPlaceID` AS `VanParkingPlaceID`,
+        `mv`.`Deleted` AS `VanDeleted`
+    FROM
+        ((`m_facility` `mf`
+        LEFT JOIN `m_van` `mv` ON ((`mv`.`FacilityID` = `mf`.`FacilityID`)))
+        LEFT JOIN `m_parkingplace` `mp` ON ((`mp`.`FacilityID` = `mf`.`FacilityID`)))
+    ORDER BY `mf`.`FacilityID`
