cdcbc8f531fba36060540844e373c659736e16b3
Paras_dba <241824862+paras-dba@users.noreply.github.com>
2026-07-07 13:14:30 +0530

    db_performance_index

---
+++ src/main/resources/db/migration/dbiemr/V89__DB_iemr_performance_index.sql
@@ -0,0 +1,23 @@
+USE db_iemr;
+
+SET @idx_exists = (
+    SELECT COUNT(*)
+    FROM information_schema.statistics
+    WHERE table_schema = 'db_iemr'
+      AND table_name = 'incentive_activity_record'
+      AND index_name = 'idx_ben_activity_created'
+);
+
+SET @sql = IF(
+    @idx_exists = 0,
+    'ALTER TABLE incentive_activity_record
+     ADD INDEX idx_ben_activity_created
+     (ben_id, activity_id, created_date),
+     ALGORITHM=INPLACE,
+     LOCK=NONE',
+    'SELECT ''idx_ben_activity_created already exists'''
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
