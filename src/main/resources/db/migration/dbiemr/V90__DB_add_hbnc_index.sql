5ee8ceba5d740697fe55d7482a729d414943f916
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-07-08 18:28:24 +0530

    add indexing in hbnc

--- src/main/resources/db/migration/dbiemr/V90__DB_add_hbnc_index.sql
+++ src/main/resources/db/migration/dbiemr/V90__DB_add_hbnc_index.sql
@@ -1,4 +1,22 @@
 USE db_iemr;
 
-CREATE INDEX idx_hbnc_visit_asha_id_id
-ON db_iemr.t_hbnc_visit (asha_id, id);
+SET @idx_exists = (
+    SELECT COUNT(*)
+    FROM information_schema.statistics
+    WHERE table_schema = 'db_iemr'
+      AND table_name = 't_hbnc_visit'
+      AND index_name = 'idx_hbnc_visit_asha_id_id'
+);
+
+SET @sql = IF(
+    @idx_exists = 0,
+    'ALTER TABLE t_hbnc_visit
+     ADD INDEX idx_hbnc_visit_asha_id_id (asha_id, id),
+     ALGORITHM=INPLACE,
+     LOCK=NONE',
+    'SELECT ''idx_hbnc_visit_asha_id_id already exists'''
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
