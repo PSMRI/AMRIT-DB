cc191008890fcff9e1663b52743c911b20e39fae
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-07-14 18:23:14 +0530

    add new column in adolescent_health

--- src/main/resources/db/migration/dbiemr/V91__add_column_adolescent.sql
+++ src/main/resources/db/migration/dbiemr/V91__add_column_adolescent.sql
@@ -7,7 +7,7 @@ SET @table = 'adolescent_health';
 SET @col = 'is_sanitary_napkin_used';
 SET @sql = (
   SELECT IF(COUNT(*) = 0,
-    CONCAT('ALTER TABLE `', @schema, '`.`', @table, '` ADD COLUMN `', @col, '` TINYINT(1) NULL DEFAULT NULL'),
+    CONCAT('ALTER TABLE `', @schema, '`.`', @table, '` ADD COLUMN `', @col, '` TINYINT(1) NOT NULL DEFAULT 0'),
     'SELECT "is_sanitary_napkin_used already exists"')
   FROM INFORMATION_SCHEMA.COLUMNS
   WHERE TABLE_SCHEMA = @schema
