78d5284988bbcc69fa845a8c4f8aa75074eab162
Saurav Mishra <saurav.mishra@bizbrolly.com>
2026-09-08 15:20:51 +0530

    rename 106 for cbac and hbnc

--- src/main/resources/db/migration/dbiemr/V106__DB_IEMR_cbac_hbnc_migration.sql
+++ src/main/resources/db/migration/dbiemr/V106__DB_IEMR_cbac_hbnc_migration.sql
@@ -116,3 +116,27 @@ SET @sql = (
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
+
+
+SET @table = 'incentive_activity_record';
+SET @col = 'is_approved';
+
+SET @sql = (
+    SELECT IF(
+        COUNT(*) = 0,
+        CONCAT(
+            'ALTER TABLE `', @schema, '`.`', @table,
+            '` ADD COLUMN `', @col,
+            '` TINYINT(1) NOT NULL DEFAULT 0'
+        ),
+        'SELECT "is_approved already exists"'
+    )
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = @schema
+      AND TABLE_NAME = @table
+      AND COLUMN_NAME = @col
+);
+
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
